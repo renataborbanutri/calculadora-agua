c66a89d2f11e74b41e420196063dd76370f4886e
renataborbanutri <169004154+renataborbanutri@users.noreply.github.com>
2024-05-05 19:23:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function calcularAgua() {
     const peso = parseFloat(document.getElementById('peso').value);
     const quantidadeAgua = peso * 0.035;
-    document.getElementById('resultado').innerHTML = `Meta de água: ${quantidadeAgua.toFixed(2)} litros`;
+    document.getElementById('resultado').innerHTML = `Quantidade de água necessária: ${quantidadeAgua.toFixed(2)} litros`;
+
+    const recomendacoes = `**Recomendações:**<br>
+        * Beba a água ao longo do dia, dividindo em pequenas porções.<br>
+        * Mantenha uma garrafa de água sempre à mão.<br>
+        * Beba água antes, durante e depois de exercícios físicos.<br>
+        * Monitore a cor da sua urina para garantir que você esteja hidratado.<br>`;
+
+    document.getElementById('recomendacoes').innerHTML = recomendacoes;
 }
