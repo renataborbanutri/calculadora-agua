e6ee08eb0dd7df45972a3467fce80981a03bae6c
renataborbanutri <169004154+renataborbanutri@users.noreply.github.com>
2024-05-05 20:14:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function calcularAgua() {
     const peso = parseFloat(document.getElementById('peso').value);
     const quantidadeAgua = peso * 0.035;
-    document.getElementById('resultado').innerHTML = `Meta de água: ${quantidadeAgua.toFixed(2)} litros`;
+    document.getElementById('resultado').innerHTML = `Meta de água: ${quantidadeAgua.toFixed(3)} litros`;
 }
