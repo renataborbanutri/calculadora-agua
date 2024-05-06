3384a0e6a2e2274f296b06d255f11180291d8924
renataborbanutri <169004154+renataborbanutri@users.noreply.github.com>
2024-05-05 19:47:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ function calcularAgua() {
     document.getElementById('resultado').innerHTML = `Meta de água: ${quantidadeAgua.toFixed(2)} litros`;
 
     const recomendacoes = `**Recomendações:**<br>
-        * Beba a água ao longo do dia, cuidado para não beber muita água perto da hora de dormir, 
-        não é bom para seu sono acordar para ir ao banheiro.<br>
-        * Mantenha uma garrafa de água sempre à mão.<br>
-        * Beba água antes, durante e depois de exercícios físicos.<br>
-        * Monitore a cor da sua urina para garantir que você esteja hidratado.<br>`;
+    * DICA DE OURO: Acorde, raspe sua língua e já comece seu dia com 500ml de água em jejum.<br>
+      * Beba a água ao longo do dia, cuidado para não beber muita água perto da hora de dormir, não é bom para seu sono acordar para ir ao banheiro.<br>
+      * Mantenha uma garrafa de água sempre à mão.<br>
+      * Monitore a cor da sua urina para garantir que você esteja hidratado, a cor ideal da urina é amarelo-clara.<br>`;
 
     document.getElementById('recomendacoes').innerHTML = recomendacoes;
 }
